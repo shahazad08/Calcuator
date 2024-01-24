720921eb317708f1f7e12c3f80458a8a6266cc63
unknown <shaikh.shahazad@bridgelabz.com>
2024-01-24 15:28:02 +0530

    Added: Subtraction functinoality added

--- src/com/bridgelabz/Calculator.java
+++ src/com/bridgelabz/Calculator.java
@@ -4,8 +4,13 @@ public class Calculator {
     public static void main(String[] args) {
         System.out.println("Welcome to Calculator");
         int a=10,b=20,c;
+        // UC 1
         c=a+b;
         System.out.println("Addition is c"+c);
+
+        // UC2
+        c=a-b;
+        System.out.println("Subtraction is c"+c);
 
     }
 }
